4260834deefc7cd0d0a31e001982f02f41b9701b
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2024-05-17 12:04:19 +0300

    upload files

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import user1 from '../../assets/user1.png'
 import card1 from '../../assets/card.png'
+import qr1 from '../../assets/QRs/1.png'
 
 
 export default function Card() {
@@ -9,7 +10,7 @@ export default function Card() {
     var rand = Math.floor(min + (Math.random() * (max - min)));
 
     let usersData = [
-        { id: 1, img: user1, code: rand, name: "عبد الرحمن ابراهيم امين عبد الحميد",nameEng:"Abdulrahman Ibrahim Amen", career: "عامل", passportNum: "A30362793", birthDate: Date("06/04/1990") },
+        { id: 1, img: user1, code: rand, name: "عبد الرحمن ابراهيم امين عبد الحميد",nameEng:"Abdulrahman Ibrahim Amen", career: "عامل", passportNum: "A30362793", birthDate: Date("06/04/1990"), qr:qr1},
     ]
     return (
         <div className='card' style={{ backgroundImage: `url(${card1})` }}>
@@ -20,7 +21,9 @@ export default function Card() {
                 <p className='arabicName'>{usersData[0].name}</p>
                 <p className='englishName'>{usersData[0].nameEng}</p>
                 <p className='id-num'>{usersData[0].passportNum}</p>
-                
+                <div className="qr1-cont">
+                    <img src={usersData[0].qr} alt="" />
+                </div>
             </div>
         </div>
     )
