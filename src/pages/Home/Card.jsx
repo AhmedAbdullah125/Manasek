0d8edf0a48073b2ba7d94c0f25ba649f71804d36
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2024-05-17 12:15:26 +0300

    upload files

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -24,6 +24,7 @@ export default function Card() {
                 <div className="qr1-cont">
                     <img src={usersData[0].qr} alt="" />
                 </div>
+                <p className='random'>{rand}</p>
             </div>
         </div>
     )
